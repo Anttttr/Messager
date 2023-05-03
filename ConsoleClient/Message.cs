b97205e97e83c1fe0c9fcc57c8ebd4c068432e64
Антон Афанасьев <antoshaafanasev@gmail.com>
2023-05-03 16:32:50 +0500

    Обозначил формат для отладки сервера

--- ConsoleClient/Message.cs
+++ ConsoleClient/Message.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleClientMessager
 {
+    [Serializable]
     public class Message
     {
         public string UserName { get; set; }
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-
+using Newtonsoft.Json;
 namespace ConsoleClientMessager
 {
     class Program
     { 
     static void Main(string[] args)
         {
-            Message msg = new Message();
+            Message msg = new Message("AAA", "BBB", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializeMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializeMsg);
             Console.WriteLine(msg.ToString());
         }
     }
