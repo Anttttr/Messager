880b3dec7eec75175bbd061dba51c5dc481c2de3
Антон Афанасьев <antoshaafanasev@gmail.com>
2023-06-10 01:48:38 +0500

    добавлены обработки post и get запосов

--- Server/Controllers/Messager.cs
+++ Server/Controllers/Messager.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using ConsoleClientMessager;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Server.Controllers
@@ -8,19 +9,33 @@ namespace Server.Controllers
     [ApiController]
     public class Messager : ControllerBase
     {
-
+        static List<Message> ListOfMessages = new List<Message>();
 
         // GET api/<Messager>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            string OutputString = "Not found";
+            if ((id < ListOfMessages.Count) && (id > 0))
+            {
+                OutputString = JsonConvert.SerializeObject(ListOfMessages[id]);
+            }
+            Console.WriteLine(String.Format("Запрошено сообщение {0} : {1}", id, OutputString));
+            return OutputString;
         }
 
         // POST api/<Messager>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult SendMessage([FromBody] Message message)
         {
+            if (message == null)
+            {
+                Console.WriteLine(message);
+                return BadRequest();
+            }
+            ListOfMessages.Add(message);
+            Console.WriteLine(String.Format("Всего: {0} Отправлено: {1}", ListOfMessages.Count, message));
+            return new OkResult();
         }
 
     }
